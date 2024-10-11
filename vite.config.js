3c0231aee348c2f8634c3465a3760be4aa19779a
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2024-10-11 11:46:27 +0300

    removed base in vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Kusini-Admin",
   plugins: [react()],
   build: {
     outDir: "docs", // GitHub Pages expects the output in 'docs'
