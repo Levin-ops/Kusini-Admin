5910bd677aea72b556fa3d1af02cd9479e25ae9a
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2024-10-27 15:45:57 +0300

    Final Build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   build: {
     outDir: "docs", // GitHub Pages expects the output in 'docs'
   },
